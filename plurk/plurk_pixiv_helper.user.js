54c6b466927aa18f7f0c60c69c1506adcbeeed5c
stu43005 <stu43005@gmail.com>
2015-01-25 01:02:12 +0800

    [Plurk Pixiv Helper]增加規則

--- plurk/plurk_pixiv_helper.user.js
+++ plurk/plurk_pixiv_helper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       Plurk Pixiv Helper
-// @version    0.7
+// @version    0.8
 // @match      http://www.plurk.com/*
 // @match      https://www.plurk.com/*
 // @require    http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
@@ -8,7 +8,7 @@
 
 function main(e) {
 	var text_holder = $(".text_holder", this);
-	var find = /\b([iI][dD]|ill|illust_id|[pP]ixiv)(\=|＝)?(\d+)(.*)/i;
+	var find = /\b([iI][dD]|ill|illust_id|[pP]ixiv|[pP]ixiv：id)(\=|＝)?(\d+)(.*)/i;
 
 	if (find.test(text_holder.text())) {
 		var childs = text_holder.get(0).childNodes;
