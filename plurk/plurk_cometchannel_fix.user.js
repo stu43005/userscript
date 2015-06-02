3958351c4b2f89618e41e02ee71dc942ef4f41b8
Shiaupiau <stu43005@gmail.com>
2015-06-02 16:15:59 +0800

    [Plurk CometChannel Fix] remove JQuery

--- plurk/plurk_cometchannel_fix.user.js
+++ plurk/plurk_cometchannel_fix.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name       Plurk CometChannel Fix
-// @version    0.2
+// @version    0.3
 // @match      http://www.plurk.com/_comet/generic?*
 // @match      https://www.plurk.com/_comet/generic?*
-// @require    http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // ==/UserScript==
 
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
 	setTimeout(function () {
 		window.location.reload()
 	}, 30000);
