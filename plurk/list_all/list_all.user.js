ee22e7f8487aecef216976024969a3507d5dccc3
Shiaupiau <stu43005@gmail.com>
2016-02-13 17:43:32 +0800

    Delete unused

--- plurk/list_all/list_all.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name           PlurkListAll
-// @namespace      Plurk
-// @description    List All Users who Liked/Limited to Read a Plurk
-// @include        http://www.plurk.com/*
-// ==/UserScript==
-
-if(location.href.match(/^http\:\/\/www\.plurk\.com\//))
-	setTimeout('(function(url){ var newRequest = document.createElement("script");newRequest.type = "text/javascript";newRequest.src = url;document.getElementsByTagName("head")[0].appendChild(newRequest); })("http://www.grassboy.tw/plurkTool/list_all/main.js")', 1000);
