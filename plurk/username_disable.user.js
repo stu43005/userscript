e85bd482cf9459a9cad32dde1a6712d06b52e463
Shiaupiau <stu43005@gmail.com>
2017-02-11 16:04:36 +0800

    [plruk]修正避免按到使用者名稱失效問題

--- plurk/username_disable.user.js
+++ plurk/username_disable.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name       避免按到使用者名稱
-// @version    1.0
+// @name       Plurk 避免按到使用者名稱
+// @version    1.1
 // @match      http://www.plurk.com/*
 // @match      https://www.plurk.com/*
 // @require    https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
@@ -8,12 +8,13 @@
 
 $("#timeline_cnt").on("click", ".name", function(e) {
 	e.preventDefault();
+	return false;
 });
 
 $(document).on("mouseenter", ".user_info", function() {
 	var name = $(".display_name", this);
 	var nick = $(".nick_name", this).text().substr(1);
 	if (name.find("a").length < 1) {
-		name.wrapInner("<a href=\"/" + nick + "\" class=\"name\"></a>");
+		name.wrapInner("<a href=\"/" + nick + "\" target=\"_blank\" class=\"name\"></a>");
 	}
 });
