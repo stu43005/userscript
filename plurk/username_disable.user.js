8cb94cb8bba41ad2733fa7b72313bcc1a5792814
Shiaupiau <stu43005@gmail.com>
2017-04-21 21:52:23 +0800

    [username_disable] 修正錯誤

--- plurk/username_disable.user.js
+++ plurk/username_disable.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name       Plurk 避免按到使用者名稱
-// @version    1.2.2
+// @version    1.2.3
 // @match      http://www.plurk.com/*
 // @match      https://www.plurk.com/*
 // @require    https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @require    https://rawgit.com/stu43005/localscript/master/index.js
-// @run-at document-end
+// @run-at     document-end
 // ==/UserScript==
 
 localScript(function() {
+	if (window != top) return;
+
 	(function($) {
 		window.PlurkElement.generate = new Proxy(window.PlurkElement.generate, {
 			apply: function(target, thisArg, argumentsList) {
