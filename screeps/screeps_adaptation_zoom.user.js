567f437e5a052680e05db11f438753f0c270342c
Shiaupiau <stu43005@gmail.com>
2017-07-08 23:04:52 +0800

    add Screeps userscripts

---
+++ screeps/screeps_adaptation_zoom.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Screeps adaptation zoom
+// @namespace    https://screeps.com/
+// @version      0.1.0
+// @author       Shiaupiau
+// @include      https://screeps.com/a/
+// @run-at       document-ready
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @require      https://github.com/Esryok/screeps-browser-ext/raw/master/screeps-browser-core.js
+// ==/UserScript==
+
+$(document).ready(() => {
+	ScreepsAdapter.onRoomChange(function (roomName) {
+		var gameFieldContainerElem = angular.element('.game-field-container');
+		var gameFieldElem = angular.element('.game-field');
+		var resizePlaceholderElem = angular.element('.resize-placeholder');
+
+		var roomElem = angular.element('.room');
+		var roomScope = roomElem.scope();
+
+		var containerWidth = gameFieldContainerElem.width();
+		var containerHeight = gameFieldContainerElem.height();
+
+		var toWidth = containerHeight;
+		var toZoom = toWidth / containerWidth * 100;
+
+		roomScope.Room.zoom = toZoom;
+	});
+});
